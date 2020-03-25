c5929c485eb0c5e3149818a7157e8caeca549746
Jago <jpturner10@icloud.com>
2020-03-25 09:29:52 +0000

    attempt at getting market data

--- jscript.js
+++ jscript.js
@@ -1,18 +1,34 @@
-function test() {
-    console.log("test 2ww")
+function getGlobalData() {
     var url = "https://corona.lmao.ninja/all"
     var xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200){
-            console.log("test 1")
-            displayData(this.response)
+            displayGlobalData(this.response)
         }
     }
     xhttp.open("GET", url, true)
     xhttp.send()
 }
 
-function displayData(data) {
+function displayGlobalData(data) {
     console.log(data)
-    document.getElementById("data").innerHTML = data
+    document.getElementById("globalDisplay").innerHTML = data
+}
+
+function getMarketData() {
+    var code = document.getElementById("marketCode").value
+    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&" + code + "=MSFT&interval=5min&apikey=EY8R9HBUQELISBSS"
+    console.log(url)
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200){
+            displayMarketData(this.response)
+        }
+    }
+    xhttp.open("GET", url, true)
+    xhttp.send()
+}
+
+function displayMarketData(data) {
+    document.getElementById("marketDisplay").innerHTML = data
 }
